af835090ca9afadaf998ca1378c29caf0c9288af
Jean <jmperez127@gmail.com>
2017-05-12 17:18:41 -0400

    Fix controller issues and add bower

--- README.md
+++
@@ -1,3 +0,0 @@
-
-This project uses the Bootstrap Startup UI Kit, kudos to the MediaLoot team.
-https://medialoot.com/item/free-bootstrap-4-theme-startup-ui-kit/
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-var express = require('express'), app = express();
+var express = require('express')
+var app = express()
 
-app.engine('pug', require('pug').__express);
-app.set('view engine', 'pug');
+app.engine('pug', require('pug').__express)
+app.set('view engine', 'pug')
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 // app.use(require('./middlewares/users'));
-app.use(require('./controllers'));
+// app.use(require('./controllers'))
 
-app.listen(3000, function() {
+require('fs').readdirSync(__dirname + '/controllers').forEach(function (file) {
+  app.use(require('./controllers/' + file))
+})
+
+app.listen(3000, function () {
   console.log('Listening on port 3000...')
-});
+})
